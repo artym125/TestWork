7c8e801a466ef382a3d66255a882317112445c3b
Ostap Artym <85745668+artym125@users.noreply.github.com>
2023-01-16 00:42:15 +0200

    Initial Commit

---
+++ TestWork/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TestWork
+//
+//  Created by Ostap Artym on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Sett"
+        
+    }
+    
+    let storyboardIns = UIStoryboard(name: "Main", bundle: nil)
+    
+//    @IBAction func buttonToSetting(_ sender: UIBarButtonItem) {
+//        let nextVC =  storyboardIns.instantiateViewController(withIdentifier: "SettingsViewControllers")
+//        self.navigationController?.pushViewController(nextVC, animated: true)
+//
+//
+//    }
+    
+    
+    
+}
